3f4efae16abe4de81c1538f98c7dd38b16e7fcca
venkatsundaraj <venkateshsundarasan@gmail.com>
2025-10-17 21:36:29 +0530

    hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
           {/* <option value={"status"}>status</option> */}
         </select>
         <button type="submit" className="bg-black">
-          submit
+          submit the value
         </button>
       </form>
       <ul className="flex flex-col">
